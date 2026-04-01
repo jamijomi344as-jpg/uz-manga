6b7d78cf96b3e740300a271e3ebe4aa8f1640ea8
jamijomi344as-jpg <jamijomi344as@gmail.com>
2026-04-01 20:54:19 +0500

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,14 +1,11 @@
 import { createClient } from '@supabase/supabase-js';
 
+// Vercel va Local muhit uchun kalitlarni tekshirish
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
 
-if (typeof window !== 'undefined') {
-  if (!supabaseUrl || !supabaseKey) {
-    console.error("DIQQAT: Supabase manzili yoki kaliti topilmadi! .env.local faylini tekshiring.");
-  } else {
-    console.log("Supabase muvaffaqiyatli ulandi ✅");
-  }
+if (!supabaseUrl || !supabaseKey) {
+  console.error("Supabase kalitlari topilmadi!");
 }
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
